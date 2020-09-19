02dde48a2eb6dc1c5883d0dd897fbc43691c0788
jyyblue <3158733490@qq.com>
2020-09-19 05:21:29 +0300

    [add] add signin page.

--- app/screens/SignInScreen.js
+++ app/screens/SignInScreen.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import {Component} from 'react';
 
-import {
-    StyleSheet,
-    View,
-    Text,
-} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicator } from 'react-native';
+import firebase from '../../database/firebase';
+
 
 export default class SignInScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            email: '', 
+            password: '',
+            isLoading: false
         }
     }
 
@@ -19,17 +19,113 @@ export default class SignInScreen extends Component {
         
     }
 
+    updateInputVal = (val, prop) => {
+        const state = this.state;
+        state[prop] = val;
+        this.setState(state);
+    }
+
+    userLogin = () => {
+        if( this.state.email === '' && this.state.password === '' ) {
+            Alert.alert("Enter details to signin!");
+        }
+        else
+        {
+            this.setState( {
+                isLoading: true,
+            });
+        }
+
+        firebase.auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then( (res) => {   
+                console.log(res);
+                console.log('User logged-in successfully!');                
+                
+                this.setState({
+                    isLoading: false,
+                    email: '',
+                    password: ''
+                });
+
+                this.props.navigation.navigate('Main');
+            })
+            .catch(error => this.setState({errorMessage: error.message})) 
+    }
+
     render() {
+        if( this.state.isLoading ) 
+        {
+            return(
+                <View style={styles.preloader}>
+                    <ActivityIndicator size="large" color="#9E9E9E"/>
+                </View>
+            )
+        }
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Signin Screen</Text>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.inputStyle}
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChangeText={(val) => this.updateInputVal(val, 'email')}
+                />
+                <TextInput
+                    style={styles.inputStyle}
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChangeText={(val) => this.updateInputVal(val, 'password')}
+                    maxLength={15}
+                    secureTextEntry={true}
+                />
+                <Button
+                    color="#3740FE"
+                    title="Signin"
+                    onPress={() => this.userLogin()}
+                />
+
+                <Text
+                    style={styles.loginText}
+                    onPress={() => this.props.navigation.navigate('Signup')}>
+                    Don't have account? Click here to signup
+                </Text>
+                
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    
-   
-});
+    container: {
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: 35,
+        backgroundColor: '#fff'
+    },
+    inputStyle: {
+        width: '100%',
+        marginBottom: 15,
+        paddingBottom: 15,
+        alignSelf: "center",
+        borderColor: "#ccc",
+        borderBottomWidth: 1
+    },
+    loginText: {
+        color: '#3740FE',
+        marginTop: 25,
+        textAlign: 'center'
+    },
+    preloader: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff'
+    }
+  });
 
