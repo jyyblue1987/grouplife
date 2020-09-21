913d1e447a5a14540854eae310c2c99c55faaf79
jyyblue <3158733490@qq.com>
2020-09-21 20:25:36 +0300

    [add] add contact info on Profile Page.

--- app/screens/profile/MemberProfilePage.js
+++ app/screens/profile/MemberProfilePage.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Component} from 'react';
 
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { Card } from 'react-native-material-ui';
 
 import { stylesGlobal } from '../../styles/stylesGlobal';
 
@@ -36,8 +37,84 @@ export default class MemberProfielPage extends Component {
                         <ActivityIndicator size="large" color="#9E9E9E"/>
                     </View>
                 }
-                <ScrollView style={{width:'100%'}}>
-                    
+                <ScrollView style={{width:'100%', paddingHorizontal: 20}}>
+                    <View style={{flex:1, flexDirection: 'row', alignItems: 'center', paddingVertical: 10}}>
+                        <View style={{justifyContent: "center"}}>
+                            <Image style = {{width: 90, height: 90, borderRadius: 45, borderColor: stylesGlobal.back_color, borderWidth: 2}} source = {require("../../assets/images/group_image.jpg")}/>
+                        </View>
+                        <View style={{width:'100%', marginLeft: 10, paddingVertical: 9}}>
+                            <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+                                Michael Snow
+                            </Text>
+
+                            <Text style={{fontSize: 17}}>
+                                Group Leader
+                            </Text>
+                        </View>    
+                    </View>
+
+                    <View style = {{width: '100%', borderWidth:0.5, borderColor:'lightgray'}} />
+
+                    <Text style={{fontWeight: 'bold', fontSize: 18, marginTop: 10}}>
+                        About Me
+                    </Text>    
+                    <Text style={{fontSize: 17, marginTop: 5}}>
+                        <Text>I have been involved with small groups for almost 15 years. I love running, hiking, and meeting new people.</Text>
+                        <Text style={{fontWeight: 'bold', color: stylesGlobal.back_color}}>   Read More</Text>
+                    </Text>                      
+
+                    <View style={{container: {borderRadius: 10}, marginTop: 30}}>
+                        <Text style={{fontWeight: 'bold', fontSize: 18, marginTop: 10}}>
+                            Contact Info
+                        </Text>
+                        {/* Email */}
+                        <View style={{flex:1, flexDirection: 'row', alignItems: 'center', paddingVertical: 10}}>
+                            <View style={{justifyContent: "center"}}>
+                                <Image style = {{width: 60, height: 60}} source = {require("../../assets/images/group_image.jpg")}/>
+                            </View>
+                            <View style={{width:'100%', marginLeft: 10, paddingVertical: 9}}>
+                                <Text style={{fontSize: 20, color:'gray'}}>
+                                    Email Address
+                                </Text>
+                                <Text style={{fontSize: 17}}>
+                                    jason.lowe@gmail.com
+                                </Text>
+                            </View>    
+                        </View>
+
+                        {/* Email */}
+                        <View style={{flex:1, flexDirection: 'row', alignItems: 'center', paddingVertical: 10}}>
+                            <View style={{justifyContent: "center"}}>
+                                <Image style = {{width: 60, height: 60}} source = {require("../../assets/images/group_image.jpg")}/>
+                            </View>
+                            <View style={{width:'100%', marginLeft: 10, paddingVertical: 9}}>
+                                <Text style={{fontSize: 20, color:'gray'}}>
+                                    Phone
+                                </Text>
+                                <Text style={{fontSize: 17}}>
+                                    313-31303131
+                                </Text>
+                            </View>    
+                        </View>
+
+                        {/* Email */}
+                        <View style={{flex:1, flexDirection: 'row', alignItems: 'center', paddingVertical: 10}}>
+                            <View style={{justifyContent: "center"}}>
+                                <Image style = {{width: 60, height: 60}} source = {require("../../assets/images/group_image.jpg")}/>
+                            </View>
+                            <View style={{width:'100%', marginLeft: 10, paddingVertical: 9}}>
+                                <Text style={{fontSize: 20, color:'gray'}}>
+                                    Address
+                                </Text>
+                                <Text style={{fontSize: 17}}>
+                                    123 Main Street
+                                </Text>
+                                <Text style={{fontSize: 17}}>
+                                    Detroit, Mi
+                                </Text>
+                            </View>    
+                        </View>
+                    </View>                 
                 </ScrollView>
             </View>
         );
