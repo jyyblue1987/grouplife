a89b71da7b30ab1ff57ae55693d5193c164b14a5
jyyblue <3158733490@qq.com>
2021-01-06 11:53:14 +0300

    [add] add swipable list view.

--- app/screens/materials/MaterialListPage.js
+++ app/screens/materials/MaterialListPage.js
@@ -1,70 +1,99 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import {Component} from 'react';
 
-import { StyleSheet, View, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text, TouchableHighlight, ActivityIndicator } from 'react-native';
 import { stylesGlobal } from '../../styles/stylesGlobal';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 import firebase from '../../../database/firebase';
 import { firestore, storage} from '../../../database/firebase';
 
-export default class MaterialListPage extends Component {
-    constructor(props) {
-        super(props);
+export default function MaterialListPage(props) {
+    const [material_list, setMaterialList] = useState(
+        Array(20)
+            .fill('')
+            .map((_, i) => ({ key: `${i}`, text: `item #${i}` }))
+    );
 
-        var group = this.props.route.params.group;
-        let uid = firebase.auth().currentUser.uid;
-
-        this.state = {
-            isLoading: false,            
-            material_list: [],          
-            edit_flag: uid == group.created_by
+    const closeRow = (rowMap, rowKey) => {
+        if (rowMap[rowKey]) {
+            rowMap[rowKey].closeRow();
         }
-    }
+    };
 
-    componentDidMount() {
-        this.initListener = this.props.navigation.addListener('focus', this.initData.bind(this));        
-    }
+    const deleteRow = (rowMap, rowKey) => {
+        closeRow(rowMap, rowKey);
+        const newData = [...material_list];
+        const prevIndex = material_list.findIndex(item => item.key === rowKey);
+        newData.splice(prevIndex, 1);
+        setMaterialList(newData);
+    };
 
-    initData = async() => {
-        console.log('MaterialListPage initData');
-        this.renderRefreshControl();
-    }
+    const onRowDidOpen = rowKey => {
+        console.log('This row opened', rowKey);
+    };
 
-    renderRefreshControl() {
-        this.setState({ isLoading: true, material_list: [] });
-
-        // this.getEventList();        
-    }
-
-    render() {
-  
-        return (
-            <View style={styles.container}>          
-                {
-                    this.state.edit_flag &&
-                    <TouchableOpacity
-                        style={{
-                            borderWidth:1,
-                            borderColor:'rgba(0,0,0,0.2)',
-                            alignItems:'center',
-                            justifyContent:'center',
-                            width:70,
-                            height:70,
-                            position: 'absolute',                                          
-                            bottom: 20,                                                    
-                            right: 20,
-                            backgroundColor:stylesGlobal.back_color,
-                            borderRadius:100,
-                            }}                        
-                            onPress={() => this.onGoCreate()}
-                        >
-                        <FontAwesome5 name="plus"  size={30} color="#fff" />
-                    </TouchableOpacity>
-                }
+    const renderItem = data => (
+        <TouchableHighlight
+            onPress={() => console.log('You touched me')}
+            style={styles.rowFront}
+            underlayColor={'#AAA'}
+        >
+            <View>
+                <Text>I am {data.item.text} in a SwipeListView</Text>
             </View>
-        );
-    }
+        </TouchableHighlight>
+    );
+
+    const renderHiddenItem = (data, rowMap) => (
+        <View style={styles.rowBack}>            
+            <TouchableOpacity
+                style={[styles.backRightBtn, styles.backRightBtnRight]}
+                onPress={() => deleteRow(rowMap, data.item.key)}
+            >
+                <Text style={styles.backTextWhite}>Delete</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <SwipeListView
+                data={material_list}
+                renderItem={renderItem}
+                renderHiddenItem={renderHiddenItem}                
+                rightOpenValue={-75}
+                previewRowKey={'0'}
+                previewOpenValue={-40}
+                previewOpenDelay={3000}
+                onRowDidOpen={onRowDidOpen}
+            />
+
+            {                
+                <TouchableOpacity
+                    style={{
+                        borderWidth:1,
+                        borderColor:'rgba(0,0,0,0.2)',
+                        alignItems:'center',
+                        justifyContent:'center',
+                        width:70,
+                        height:70,
+                        position: 'absolute',                                          
+                        bottom: 20,                                                    
+                        right: 20,
+                        backgroundColor:stylesGlobal.back_color,
+                        borderRadius:100,
+                        }}                        
+                        onPress={() => this.onGoCreate()}
+                    >
+                    <FontAwesome5 name="plus"  size={30} color="#fff" />
+                </TouchableOpacity>
+            }
+        </View>
+    );
+   
+    
 }
 
 
@@ -76,5 +105,41 @@ const styles = StyleSheet.create({
         paddingVertical: 20,
         paddingHorizontal: 10,
     },  
+
+    backTextWhite: {
+        color: '#FFF',
+    },
+    rowFront: {
+        alignItems: 'center',
+        backgroundColor: '#CCC',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        justifyContent: 'center',
+        height: 50,
+    },
+    rowBack: {
+        alignItems: 'center',
+        backgroundColor: '#DDD',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightBtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 75,
+    },
+    backRightBtnLeft: {
+        backgroundColor: 'blue',
+        right: 75,
+    },
+    backRightBtnRight: {
+        backgroundColor: 'red',
+        right: 0,
+    },
     
 });
