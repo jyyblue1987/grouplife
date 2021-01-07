780d941bda92619c88bbc0e37f3f482ec1951e05
jyyblue <3158733490@qq.com>
2021-01-07 12:26:47 +0300

    [add] add text editor.

--- app/screens/materials/MaterialCreatePage.js
+++ app/screens/materials/MaterialCreatePage.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Component} from 'react';
 
-import { StyleSheet, View, TextInput, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import RadioButtonRN from 'radio-buttons-react-native';
 import { Card } from 'react-native-material-ui';
+import {actions, getContentCSS, RichEditor, RichToolbar} from 'react-native-pell-rich-editor';
 
 import { stylesGlobal } from '../../styles/stylesGlobal';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -16,13 +17,52 @@ import { Icon } from 'react-native-material-ui';
 
 export default function MaterialCreatePage(props) {
     const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
     const material_type_list = [
         { label: 'Freeform Text', type: 1},
         { label: 'Upload File', type: 2 }
     ];
 
+    const initHTML = `<br/>
+            <center><b onclick="_.sendEvent('TitleClick')" id="title" contenteditable="false">Rich Editor</b></center>
+            <center>
+            <a href="https://github.com/wxik/react-native-rich-editor">React Native</a>
+            <span>And</span>
+            <a href="https://github.com/wxik/flutter-rich-editor">Flutter</a>
+            </center>
+            <br/>            
+            <br/>Click the picture to switch<br/><br/>
+            `;
+
+    const richText = React.createRef();
+
+    useEffect(() => {
+        setContent("Hello <b>World</b> <p>this is a new paragraph</p> <p>this is another new paragraph</p>")        
+    })
+
     const onSelectType = (type) => {
         console.log(type);
+    }
+
+    const onEditorInitialized = () => {
+        console.log("onEditorInitialized");
+    }
+
+    const onChangeHTML = (html) => {
+        consoe.log(html);
+    }
+
+    const handleKeyUp = data => {
+        console.log('Keyup:', data);
+    }
+
+    const onSaveMaterial = async() => {
+        let html = await richText.current?.getContentHtml();
+        console.log(html);
+    }
+
+    const onCancelMaterial = () => {                
+        richText.current?.setContentHTML(initHTML);
     }
 
     return (
@@ -48,10 +88,31 @@ export default function MaterialCreatePage(props) {
                         style={[stylesGlobal.inputStyle, {marginTop: 35, marginLeft: 20}]}
                         placeholder="Title"
                         autoCapitalize = 'none'
-                        value={title}
+                        value={title}                        
                         onChangeText={(val) => setTitle(val)}
                     />
-                
+
+                <RichEditor
+                    ref={richText}
+                    initialContentHTML={initHTML}
+                    editorInitializedCallback={() => onEditorInitialized()}
+                    onChange={(html) => onChangeHTML(html)}
+                    onFocus={() => onChangeHTML("Focus")}
+                    onKeyUp={(data) => handleKeyUp(data)}
+                    />
+
+                <View style={{width: '100%', flexDirection: 'row'}}>
+                    <TouchableOpacity style = {{flex: 1, height: 40, backgroundColor: stylesGlobal.back_color, justifyContent: 'center', alignItems: 'center', marginHorizontal: 5}} 
+                        onPress = {() => onSaveMaterial()}>
+                        <Text style = {[stylesGlobal.general_font_style, {color: '#fff', fontSize: 16}]}>Save</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style = {{flex: 1, height: 40, backgroundColor: stylesGlobal.back_color, justifyContent: 'center', alignItems: 'center', marginHorizontal: 5}} 
+                        onPress = {() => onCancelMaterial()}>
+                        <Text style = {[stylesGlobal.general_font_style, {color: '#fff', fontSize: 16}]}>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
+                    
 
             </KeyboardAwareScrollView>
         </View>
