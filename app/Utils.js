8ac4c8568bec4cd15c2f887984fe26d32d52d776
jyyblue <3158733490@qq.com>
2021-01-12 05:37:01 +0300

    [fixed] auto detect url link

--- app/Utils.js
+++ app/Utils.js
@@ -1,6 +1,10 @@
 export function linkify(text) {
     var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
     return text.replace(urlRegex, function(url) {
+        var n = text.indexOf('<a href="' + url + '"');
+        if( n >= 0 )
+            return url;
+        
         return '<a href="' + url + '">' + url + '</a>';
     });
 }
